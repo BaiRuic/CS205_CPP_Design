5f70f4cada38f0e086fc3c26fc9f993cf61f4818
Bairuic <brc730@outlook.com>
2022-12-16 21:07:16 +0800

    finash basic function

--- project4/main.cpp
+++ project4/main.cpp
@@ -1,7 +1,16 @@
 #include"matrix.hpp"
 #include<iostream>
+using namespace std;
 
 int main(){
-    Matrix<int> mat_a(4, 5, 5);
-    Matrix<int> mat_b = mat_a;
+
+    Matrix<int> mat_a(2, 5, 5);
+    
+    Matrix<int> mat_b(5, 2, 2);
+    Matrix<int> mat_c = mat_a * mat_b;
+    mat_c *= mat_c;
+    mat_c.info();
+
+
+    return 0;
 }
